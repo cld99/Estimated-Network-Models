Fix covariance modeling structure

estimate_covariance.py
@@ -3,74 +3,64 @@ import torch
 # Set random seed
 torch.manual_seed(42)
 
+# Set datatype to float64 to reduce errors due to precision
+torch.set_default_dtype(torch.float64)
+
+def print_evals(matrix):
+    evals = torch.linalg.eigvalsh(matrix)
+    print(f"{evals[0]}, {evals[-1]}")
+
+def wishart_sample(df, covariance_matrix):
+    p = covariance_matrix.size(dim=0)
+    dist = torch.distributions.MultivariateNormal(torch.zeros(p), covariance_matrix=covariance_matrix)
+    G = dist.sample((df,))
+    # print(G)
+    return G.T @ G + torch.eye(p) * 1e-4
+
 class CovarianceModel():
-    def __init__(self, p, d, alpha, sigma_Z, sigma_theta):
+    def __init__(self, p, d, df, sigma_Z):
         """
         p: dimension of X
         d: dimension of latent space
-        alpha: baseline affinity for covariances
+        df: degrees of freedom in the Inverse-Wishart distribution
         sigma_Z: standard deviation of latent positions z
-        sigma_theta: standard deviation of correlation entries theta for observed data
         """
         self.p = p
         self.d = d
+        if df < d:
+            raise ValueError("Invalid degrees of freedom provided (must be at least p)")
+        self.df = df
         self.sigma_Z = sigma_Z
-        self.sigma_theta = sigma_theta
 
-        self.alpha = torch.tensor(torch.ones(1) * alpha, requires_grad=True)
         self.Z = torch.tensor(torch.randn((p, d)) * sigma_Z, requires_grad=True)
-        self.stdev = torch.ones(p, requires_grad=True)
-        self.theta_entries = torch.tensor(self._gen_theta_entries() + torch.randn(p * (p - 1) // 2) * sigma_theta, requires_grad=True)
-        # self.theta_entries = torch.tensor(self._gen_theta_entries() + torch.randn(p * (p + 1) // 2) * sigma_theta, requires_grad=True)
+        self.theta = torch.tensor(self._gen_theta_entries(), requires_grad=True)
     
     def _gen_theta_entries(self):
-        theta_entries = torch.zeros(self.p * (self.p - 1) // 2)
-        # theta_entries = torch.zeros(self.p * (self.p + 1) // 2)
-        theta_index = 0
-        latent_dist = torch.cdist(self.Z, self.Z)
-        # for j in range(self.p):
-            # for k in range(j, self.p):
-        for j in range(self.p-1):
-            for k in range(j+1, self.p):
-                affinity = self.alpha - latent_dist[j,k] ** 2
-                theta_entries[theta_index] = 2 * torch.sigmoid(affinity) - 1
-                # theta_entries[theta_index] = affinity
-                theta_index += 1
-        return theta_entries.detach()
+        # print_evals(self.Z @ self.Z.T)
+        Psi = self.Z @ self.Z.T + torch.eye(self.p) * 1e-4
+        # print_evals(Psi)
+        L1 = torch.linalg.cholesky(Psi)
+        Psi_inv = torch.cholesky_inverse(L1)
+        # print_evals(Psi_inv)
+        dist = torch.distributions.wishart.Wishart(df=self.df, covariance_matrix=Psi_inv)
+        sample = dist.rsample()
+        # sample = wishart_sample(self.df, covariance_matrix=Psi_inv)
+        # print_evals(sample)
+        L2 = torch.linalg.cholesky(sample)
+        # theta = torch.linalg.inv(L2).T
+        theta = torch.linalg.cholesky(torch.cholesky_inverse(L2))
+        return torch.round(theta, decimals=4).detach()
     
     def get_cov(self):
-        corr = torch.eye(self.p)
-        # theta = torch.zeros((self.p, self.p))
-        theta_index = 0
-        # for j in range(self.p):
-        #     for k in range(j, self.p):
-        #         theta[j,k] = self.theta_entries[theta_index]
-        #         theta[k,j] = theta[j,k]
-        for j in range(self.p-1):
-            for k in range(j+1, self.p):
-                corr[j,k] = self.theta_entries[theta_index]
-                corr[k,j] = corr[j,k]
-                theta_index += 1
-        cov = torch.diag(self.stdev) @ corr @ torch.diag(self.stdev)
-        # return cov
-        return self._validate_cov(cov)
-        # return self._validate_cov(theta)
-    
-    def _validate_cov(self, cov):
-        # for _ in range(2):
-        eigvals, eigvecs = torch.linalg.eigh(cov)
-        eigvals_clipped = torch.clamp(eigvals, min=1e-6)
-        cov = eigvecs @ torch.diag(eigvals_clipped) @ eigvecs.T + torch.eye(self.p) * 1e-6
-        cov = (cov + cov.T) / 2
-        return cov
+        return self.theta @ self.theta.T + torch.eye(self.p) * 1e-4
     
     def get_model_params(self):
-        return [self.alpha, self.Z, self.stdev, self.theta_entries]
-        # return [self.alpha, self.Z, self.theta_entries]
+        return [self.Z, self.theta]
     
     # Generate n samples according to given covariance matrix
     def gen_samples(self, n):
-        dist = torch.distributions.MultivariateNormal(torch.zeros(self.p), covariance_matrix=self.get_cov())
+        dist = torch.distributions.MultivariateNormal(torch.zeros(self.p), scale_tril=self.theta + torch.eye(self.p) * 1e-4)
+        # dist = torch.distributions.MultivariateNormal(torch.zeros(self.p), covariance_matrix=self.get_cov())
         return dist.sample((n,))
     
     # Log likelihood of latent positions given variance
@@ -78,47 +68,46 @@ class CovarianceModel():
         dist = torch.distributions.MultivariateNormal(torch.zeros(self.d), covariance_matrix=torch.eye(self.d) * self.sigma_Z)
         return torch.sum(dist.log_prob(self.Z), dim=None)
     
-    # Log likelihood of covariance matrix entries given latent positions and variance
+    # Log likelihood of covariance matrix entries given latent positions and degrees of freedom
     def _theta_llk(self):
-        distribution = torch.distributions.Normal(0, scale=self.sigma_theta)
-        latent_dist = torch.cdist(self.Z, self.Z)
-        cov = self.get_cov()
-        llk = 0
-        # for j in range(self.p):
-            # for k in range(j, self.p):
-        for j in range(self.p-1):
-            for k in range(j+1, self.p):
-                affinity = self.alpha - latent_dist[j,k] ** 2
-                llk = llk + distribution.log_prob(cov[j,k] - 2 * torch.sigmoid(affinity) - 1)
-                # llk = llk + distribution.log_prob(cov[j,k] - affinity)
+        Psi = self.Z @ self.Z.T + torch.eye(self.p) * 1e-4
+        L1 = torch.linalg.cholesky(Psi)
+        Psi_inv = torch.cholesky_inverse(L1)
+        dist = torch.distributions.wishart.Wishart(df=self.df, covariance_matrix=Psi_inv)
+        # L2 = torch.linalg.cholesky(self.get_cov())
+        # cov_inv = torch.cholesky_inverse(L2)
+        cov_inv = torch.cholesky_inverse(self.theta)
+        llk = dist.log_prob(cov_inv) - (self.d + 1) * torch.logdet(self.get_cov())
         return llk
     
     # Log likelihood of observed data given covariance matrix
     def _X_llk(self, X):
-        # cov = self.get_cov()
-        # print(torch.allclose(cov, cov.T))
-        # print(torch.min(torch.linalg.eigvalsh(cov)))
-        # print(torch.isfinite(cov).all())
-        dist = torch.distributions.MultivariateNormal(torch.zeros(self.p), covariance_matrix=self.get_cov())
+        dist = torch.distributions.MultivariateNormal(torch.zeros(self.p), scale_tril=self.theta + torch.eye(self.p) * 1e-4)
+        # dist = torch.distributions.MultivariateNormal(torch.zeros(self.p), covariance_matrix=self.get_cov())
         return torch.sum(dist.log_prob(X), dim=None)
 
     # Compute loss as negative log likelihood of observed data given model parameters
     def loss(self, X):
         return -self._Z_llk() - self._theta_llk() - self._X_llk(X)
 
-model = CovarianceModel(5, 5, 5, 1, 0.01)
-print(model.get_cov())
+model = CovarianceModel(500, 10, 500, 1)
+# print("Initial Covariance:")
+# print(model.get_cov())
 
-model_2 = CovarianceModel(5, 5, 5, 1, 0.01)
-print(model_2.get_cov())
+target_model = CovarianceModel(500, 10, 500, 1)
+# print("Target Covariance:")
+# print(target_model.get_cov())
 
-data = model_2.gen_samples(250)
+data = target_model.gen_samples(100)
 
-optim = torch.optim.Adam(model.get_model_params())
+# print("Sample covariance:")
+# print(torch.cov(data.T))
+
+optim = torch.optim.Adam(model.get_model_params(), lr=0.01)
 
 loss = model.loss(data)
 
-for _ in range(100):
+for i in range(10000):
     optim.zero_grad()
 
     loss = model.loss(data)
@@ -126,6 +115,18 @@ for _ in range(100):
     loss.backward()
     optim.step()
 
-print(model.get_cov())
+    if i % 1000 == 0:
+        print("Fitted model:")
+        print(torch.linalg.matrix_norm(target_model.get_cov() - model.get_cov()) ** 2)
 
-print(torch.cov(data.T))
+# print("Fitted Covariance:")
+# print(model.get_cov())
+
+print("Sample covariance:")
+print(torch.linalg.matrix_norm(target_model.get_cov() - torch.cov(data.T)) ** 2)
+
+print("Fitted model:")
+print(torch.linalg.matrix_norm(target_model.get_cov() - model.get_cov()) ** 2)
+
+# sample = wishart_sample(df=5, covariance_matrix=torch.tensor([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]]))
+# print_evals(sample)